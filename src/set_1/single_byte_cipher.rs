dd8395073453b48f0c1900dd1ea5346e9c6ebb2b
Ben Schofield <me@benjscho.dev>
2023-09-10 08:57:58 -0700

    Set 1 ch 4 - detect single-char XOR
    The challenges definitely build nicely on top of each other.
    
    I think after completing set 1 I'll refactor all of the functions
    created into some logical util modules.

--- src/set_1/single_byte_cipher.rs
+++ src/set_1/single_byte_cipher.rs
@@ -1,9 +1,9 @@
-use std::{str::from_utf8, vec};
+use std::{str::from_utf8, vec, path::Path, fs::File, io::{BufReader, BufRead}};
 
 use super::{fixed_xor::fixed_xor, hex_to_base64::hex_to_bytes};
 
 /// Find one byte cipher candidates using a scoring of plaintext.
-pub fn find_cipher_candidates(input: &[u8]) -> Vec<u8> {
+pub fn solve_one_byte_cipher(input: &[u8]) -> Vec<u8> {
     let mut max_score: i32 = i32::MIN;
     let mut candidate: u8 = 0;
     let mut candidate_pt: Vec<u8> = vec![];
@@ -16,10 +16,9 @@ pub fn find_cipher_candidates(input: &[u8]) -> Vec<u8> {
             candidate = c;
             candidate_pt = decrypted.clone();
 
-            eprintln!("New candidate key: {}, translated text: {}", &c, from_utf8(&decrypted).unwrap());
+            //eprintln!("New candidate key: {}, translated text: {}", &c, from_utf8(&decrypted).unwrap());
         }
     }
-    println!("Final candidate: {}, final translation: {}", &candidate, from_utf8(&candidate_pt).unwrap());
     candidate_pt
 }
 
@@ -40,6 +39,22 @@ fn english_score(input: &[u8]) -> i32 {
     score
 }
 
+fn find_in_file(file: &Path) -> Vec<u8> {
+    let mut reader = BufReader::new(File::open(file).expect("Can't open file."));
+    let mut max_score = i32::MIN;
+    let mut found_str = vec![];
+    for line in reader.lines() {
+        let candidate = solve_one_byte_cipher(&line.unwrap().as_bytes());
+        let score = english_score(&candidate);
+        if score > max_score {
+            max_score = score;
+            found_str = candidate;
+        }
+    }
+    println!("Final candidate: {}", from_utf8(&found_str).unwrap());
+    found_str
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -48,7 +63,15 @@ mod tests {
     fn one_byte_cipher_happy() {
         let input = "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736".as_bytes();
         let expected = "Cooking MC's like a pound of bacon".as_bytes();
-        let actual = find_cipher_candidates(&input);
+        let actual = solve_one_byte_cipher(&input);
+        assert_eq!(expected, actual);
+    }
+
+    #[test]
+    fn find_in_file_happy() {
+        let file = Path::new("./data/set_1/ch4.txt");
+        let expected = "Now that the party is jumping\n".as_bytes();
+        let actual = find_in_file(file);
         assert_eq!(expected, actual);
     }
 }
